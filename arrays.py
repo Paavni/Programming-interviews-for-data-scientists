609d865c867eda25118f3c99d60321933f73086b
Paavni Rattan <rattan.paavni@gmail.com>
2015-02-28 16:07:07 -0800

    Add files

--- arrays.py
+++ arrays.py
@@ -2,5 +2,20 @@
 
 #sum of x and y is z
 
-def issum(x):
+def issumbruteforce(x, sum):
+	l = length(x)
+	found = 0
+	for i in xrange(l):
+		for j in xrange(l);
+			if i!=j:
+				if x[i]+x[j]==sum:
+					found +=1
+					print "sum of %d and %d is %d" %(x[i], x[j], sum)
+	print "No of pairs found is %d" %(found)
+
+def sortandfind(x,sum):
+
+	def sortnos(x):
+		
+
 	
